da4814b289540587257d1b7289329cc21a3b08dd
Phatcharapong Jullamonton <whitecanze123@gmail.com>
2021-07-16 19:20:30 +0700

    Fix 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect, HashRouter } from 'react-router-dom'
 import Generator from './components/generator'
 import Form from './components/Form'
 
 function App() {
 
   return (
-    <Router>
+    <HashRouter hashType="noslash">
       <Switch>
           <Route exact path="/" component={Generator} />
           <Route exact path="/cal" component={Form} />
-          <Route path="/generator"><Redirect to="/" /></Route> 
-          <Route><Redirect to="/" /></Route> 
+          <Route>
+            <Redirect to="/" />
+          </Route> 
       </Switch>
-    </Router>
+    </HashRouter>
   )
 }
 
